768986a80cc8e0d50423fe48bbd2a59dacfc188c
Mahsa <mahsahaddadi19@gmail.com>
2024-10-11 14:36:31 +1100

    Initial commit after cleaning up large files

---
+++ routes/userRoutes.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const userController = require('./../controllers/userControllers');
+const authController = require('./../controllers/authControllers');
+
+
+
+const router = express.Router();
+
+
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+
+
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+
+router.patch('/updateMyPassword', authController.protect, authController.updatePassword);
+
+
+router.patch('/updateMe', authController.protect, userController.updateMe);
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
+
+
+
+
+router
+    .route('/')
+    .get(userController.getAllUsers)
+    .put(userController.createUser);
+
+
+router
+    .route('/:id')
+    .get(userController.getUser)
+    .patch(userController.updateUser)
+    .delete(userController.deleteUser);
+
+
+module.exports = router;
