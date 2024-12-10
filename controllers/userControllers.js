0fe62d9ac67a5a75fdc452dc03d8f7f13784566d
Mahsa <mahsahaddadi19@gmail.com>
2024-12-10 18:44:16 +1100

    Authentication

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -14,7 +14,12 @@ const filterObj = (obj, ...allowedFields) => {
 
 };
 
+exports.getMe = (req, res, next) => {
+    
+    req.params.id = req.user.id;
 
+    next();
+};
 
 
 exports.updateMe = catchAsync(async (req,res, next) => {
